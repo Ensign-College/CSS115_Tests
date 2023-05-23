732e69a3c8368902af63bb7a256fac404a1ec3c7
Ismatulla Mansurov <47342870+sapoepsilon@users.noreply.github.com>
2023-05-23 13:32:47 -0600

    fix typo

--- CS115_lab4a_check_output.py
+++ CS115_lab4a_check_output.py
@@ -1,17 +1,28 @@
 import os
+import re
 import subprocess
 
-def check_output(output, expected_output):
-    # Remove all styling (whitespaces, newlines, and punctuation)
-    output = ''.join(e for e in output if e.isalnum())
-    expected_output = ''.join(e for e in expected_output if e.isalnum())
+def check_same_words_ignore_numbers(input2):
+    input1 = """
+        Candy Corn               $0.50    [Tax: $0.04]
+        Gummy Bears              $0.10    [Tax: $0.01]
+        Chocolate Chip           $2.20    [Tax: $0.16]
+        Pistachio                $1.70    [Tax: $0.12]
+        Vanilla                  $3.50    [Tax: $0.25]
+        Oatmeal Raisin           $0.60    [Tax: $0.04]
+        ----------------------------------------------
+        Order Subtotals:         $8.60    [Tax: $0.62]
+        Order Total:             $9.22
+        Candy Corn               $0.38    [Tax: $0.03]
 
-    # Convert both strings to lowercase
-    output = output.lower()
-    expected_output = expected_output.lower()
+        Total number of items in order: 6
+    """
+    input1_words = set(re.findall(r'\b\w+\b', re.sub(r'\d', '', input1)))
+    input2_words = set(re.findall(r'\b\w+\b', re.sub(r'\d', '', input2)))
+    
+    # Check whether all words from input2 are in input1
+    return input2_words.issubset(input1_words)
 
-    # Check if the modified output matches the modified expected output
-    return output == expected_output    
 def run_job(cmd, input_data=None):
     current_directory = os.getcwd()
     ret = subprocess.run(
@@ -46,9 +57,10 @@ if __name__ == "__main__":
     else:
         run_command = "java -cp . DessertShop"
         run_result = run_job(run_command)
-        print(f"{run_result}")
-        if check_output(run_result) == True: 
+        if check_same_words_ignore_numbers(run_result):
             print("OK")
+        else:
+            print("Make sure to match example output in the README")
 
     # Remove .class files
     remove_class_files()
